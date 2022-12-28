c33882edda71ba3d3f293031fabc063a6bc881f1
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2022-12-28 15:16:55 +1300

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!(typeof a === 'number') ||  !(typeof b === 'number')) {
+        return 'ERROR';
+    } else if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
 
+    let start, end;
+    if (a <= b) {
+        start = a;
+        end = b;
+    } else {
+        start = b;
+        end = a;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+sumAll(1, 2);
 
 // Do not edit below this line
 module.exports = sumAll;
