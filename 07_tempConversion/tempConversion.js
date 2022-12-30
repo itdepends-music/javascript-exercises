6d71bfa05ac1f7a3d436050707a9f8809065dfc4
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2022-12-30 19:49:54 +1300

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  celsius = (f - 32) * 5/9;
+  rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  fahrenheit = (c * 9/5) + 32;
+  rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
