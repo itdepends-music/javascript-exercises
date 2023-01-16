50398d41ee53337fddb620e8d7a5f975d9d53a97
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2023-01-16 20:12:13 +1300

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((a, b) => {
+        aDeath = "yearOfDeath" in a ? a.yearOfDeath : new Date().getFullYear();
+        bDeath = "yearOfDeath" in b ? b.yearOfDeath : new Date().getFullYear();
 
+        ageA = aDeath - a.yearOfBirth;
+        ageB = bDeath - b.yearOfBirth;
+        return ageA > ageB ? a : b;
+    });
 };
 
 // Do not edit below this line
