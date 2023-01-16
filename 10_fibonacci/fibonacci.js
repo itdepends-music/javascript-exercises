bd99bee5cce7de613ef1d89a4156615d6be2de68
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2023-01-16 19:51:09 +1300

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n <= 0) {
+        return "OOPS";
+    }
 
+    let fib1 = 1;
+    let fib2 = 1;
+    for (let i = 0; i < n - 2; i++) {
+        let temp = fib1;
+        fib1 = fib1 + fib2;
+        fib2 = temp;
+    }
+    return fib1;
 };
 
 // Do not edit below this line
