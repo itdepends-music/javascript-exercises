b75490b428ce35026971b8f824d8db17ec333c8f
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2023-01-16 19:28:43 +1300

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[^\w]/g, "").toLowerCase();
+    const reverse = word.split('').reverse().join('');
+    return word === reverse;
 };
 
 // Do not edit below this line
